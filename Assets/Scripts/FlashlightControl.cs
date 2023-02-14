54eafd0684d102bb29018de6cf69fdb43227a7c7
Nathan-Kurtz <kn24505@students.mcpasd.k12.wi.us>
2023-02-14 11:23:19 -0600

    Flashlight and stuff

---
+++ Assets/Scripts/FlashlightControl.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightControl : MonoBehaviour
+{
+
+    public GameObject light;
+    private bool toggle;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        toggle = false;
+        light.SetActive(toggle);
+    }
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.F)){
+            ToggleLight();
+            Debug.Log("Clicked");
+        }
+
+
+
+        if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+        {
+            
+            light.transform.rotation = Quaternion.Euler(0, 0, 135);
+        }
+        else if(Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.A)){
+             light.transform.rotation = Quaternion.Euler(0, 0, 180);
+        }
+        else if(Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S)){
+            light.transform.rotation = Quaternion.Euler(0, 0, 90);
+        }
+        
+
+
+            if (Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S))
+            {
+                
+                light.transform.rotation = Quaternion.Euler(0, 0, 45);
+            }
+            else if(Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A)){
+                light.transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+            else if(Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S)){
+                light.transform.rotation = Quaternion.Euler(0, 0, 90);
+            }
+
+
+
+
+           if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.S))
+            {
+                
+                light.transform.rotation = Quaternion.Euler(0, 0, -45);
+            }
+            else if(Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A)){
+                light.transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+            else if(Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S)){
+                light.transform.rotation = Quaternion.Euler(0, 0, -90);
+            }
+
+
+
+              if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.W))
+            {
+                
+                light.transform.rotation = Quaternion.Euler(0, 0, -135);
+            }
+            else if(Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A)){
+                light.transform.rotation = Quaternion.Euler(0, 0, 180);
+            }
+            else if(Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.W)){
+                light.transform.rotation = Quaternion.Euler(0, 0, -90);
+            }
+
+
+
+      
+    }
+    public void ToggleLight(){
+        toggle = !toggle;
+        Debug.Log(toggle);
+        light.SetActive(toggle);
+    }
+    
+}
